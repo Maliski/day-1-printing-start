a120ff2ded2bcaa1904105141bfb80ea2168adbd
Maliski <86474495+Maliski@users.noreply.github.com>
2021-08-23 20:18:21 +0000

    131415

--- main.py
+++ main.py
@@ -10,3 +10,8 @@ print("Day 1" + "-"" String Manipulation")
 print('String Concatenation is done with the "+" sign.')
 print('e.g. print("Hello" + "world")')
 print("New lines can be created with a backslash and n.")
+input("what is your name?")
+input("a promt for the user.")
+print("Hello Josie" + input("what is your name?"))
+
+
